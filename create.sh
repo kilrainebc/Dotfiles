762ff05b8906e0f0921207fd42974f9b4da0473e
kilrainebc <35840407+kilrainebc@users.noreply.github.com>
2019-12-28 23:26:09 -0500

    updates to create.sh

--- create.sh
+++ create.sh
@@ -4,17 +4,17 @@ echo ""
 #echo "=== COPYING DOTFILES ==="
 echo ""
 
-#cd ~
-#cp ~/.Xresources ~/.dotfiles/
-#cp ~/.bash_profile ~/.dotfiles
-#cp ~/.aliasrc ~/.dotfiles
-#cp ~/.bashrc ~/.dotfiles
-#cp -r ~/.config/ ~/.dotfiles
-#cp ~/.fehbg ~/.dotfiles
-#cp ~/.vimrc ~/.dotfiles
-#cp ~/.xinitrc ~/.dotfiles
-#cp ~/.gitconfig ~/.dotfiles
-#cp ~/bg1.png ~/.dotfiles
+cd ~
+cp ~/.Xresources ~/.dotfiles/
+cp ~/.bash_profile ~/.dotfiles
+cp ~/.aliasrc ~/.dotfiles
+cp ~/.bashrc ~/.dotfiles
+cp -r ~/.config/ ~/.dotfiles
+cp ~/.fehbg ~/.dotfiles
+cp ~/.vimrc ~/.dotfiles
+cp ~/.xinitrc ~/.dotfiles
+cp ~/.gitconfig ~/.dotfiles
+cp ~/bg1.png ~/.dotfiles
 
 
 echo ""
@@ -39,7 +39,7 @@ ln -sv ~/.dotfiles/.Xresources ~
 ln -sv ~/.dotfiles/.bash_profile ~
 ln -sv ~/.dotfiles/.aliasrc ~
 ln -sv ~/.dotfiles/.bashrc ~
-ln -sv ~/.dotfiles/.config/ ~
+ln -sdv ~/.dotfiles/.config/ ~
 ln -sv ~/.dotfiles/.fehbg ~
 ln -sv ~/.dotfiles/.vimrc ~
 ln -sv ~/.dotfiles/.xinitrc ~
