91110c066e0ba4e2676d5c0ee125929e35fb6974
kilraine.b.c <35840407+kilrainebc@users.noreply.github.com>
2021-07-03 20:09:37 -0400

    making more extensible

--- dotcfg.sh
+++ dotcfg.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
+if [[ -z $1 ]]; then
+  printf "usage: %s <DISTRO> " $(basename $0)
+  exit 64
+fi
 
-printf "\n[+] Cleaning Dotfiles\n"
-rm ~/.Xresources
-rm ~/.bash_profile
-rm ~/.aliasrc
-rm ~/.bashrc
-rm -rf ~/.config/
-rm ~/.fehbg
-rm ~/.vimrc
-rm ~/.xinitrc
-rm ~/.gitconfig
-rm ~/bg1.png
+if [[ ! -d ./"$1" ]]; then
+  printf "distro provided ("$1") does not have a configuration directory."
+  exit 64
+fi
 
-printf "\n[+] Symlinking Dotfiles\n"
-ln -sv $(pwd)/.Xresources ~
-ln -sv $(pwd)/.bash_profile ~
-ln -sv $(pwd)/.aliasrc ~
-ln -sv $(pwd)/.bashrc ~
-cp -rv $(pwd)/.config ~/.config ~
-ln -sv $(pwd)/.fehbg ~
-ln -sv $(pwd)/.vimrc ~
-ln -sv $(pwd)/.xinitrc ~
-ln -sv $(pwd)/.gitconfig ~
-cp ./bg1.png ~/bg1.png
-
-function 
-
-wal -i ~/bg1.png
-startx
+source ./"$1"/install.bash
+main
